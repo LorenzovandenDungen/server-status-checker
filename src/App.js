dfcfd4a329624b3489984409b230150ab0c7920b
LorenzovandenDungen <2130766@talnet.nl>
2023-05-24 12:19:21 +0200

    save

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ServerList from "./components/ServerList";
 function App() {
   return (
     <div>
+      {/* Update the path to your static files */}
+      <img src={process.env.PUBLIC_URL + "/static/image.png"} alt="Logo" />
       <ServerList />
     </div>
   );

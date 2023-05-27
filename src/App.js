773a457875f5a60eebfeaec110653590f6ae92a7
LorenzovandenDungen <2130766@talnet.nl>
2023-05-27 21:00:47 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ServerList from "./components/ServerList";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import Home from "./components/Home";
@@ -8,9 +7,12 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-      {/* Update the path to your static files */}
-      <img src={process.env.PUBLIC_URL + "/static/image.png"} alt="Logo" />
-      <ServerList />
+      <>
+      <Header />
+      <Home />
+      <SideNav />
+      <Footer />
+      </>
     </div>
   );
 }

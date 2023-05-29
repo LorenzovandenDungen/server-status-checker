0c05d8e700ff9c0a7c12c615988e6c32b3d58b59
LorenzovandenDungen <2130766@talnet.nl>
2023-05-29 10:51:50 +0200

    save

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-reportWebVitals();

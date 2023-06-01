bcfaa242fb80dcd0f02bb6e7f79f44fc9b2494c9
Your Name <Lorenzovandendungen123456@gmail.com>
2023-06-01 23:32:20 +0200

    save

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,73 @@
-<div>
-  import React from 'react'
-  const Header = () =&gt; {'{'}
+import React from 'react';
+
+const Header = () => {
   return (
-  <div>
-    <div className="wrapper">
-      {/* Navbar */}
-      <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-        {/* Left navbar links */}
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" /></a>
-          </li>
-          <li className="nav-item d-none d-sm-inline-block">
-            <a href="#" className="nav-link">Home</a>
-          </li>
-          <li className="nav-item d-none d-sm-inline-block">
-            <a href="#" className="nav-link">Contact</a>
-          </li>
-        </ul>
-        {/* Right navbar links */}
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#" data-toggle="dropdown">
-              <i className="far fa-comments" />
-              <span className="badge badge-danger navbar-badge">3</span>
-            </a>
-            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-              <a href="#" className="dropdown-item">
-                {/* Message Start */}
-                <div className="media">
-                  <img src="dist/img/user1-128x128.jpg" alt="User Avatar" className="img-size-50 mr-3 img-circle" />
-                  <div className="media-body">
-                    <h3 className="dropdown-item-title">
-                      Brad Diesel
-                      <span className="float-right text-sm text-danger"><i className="fas fa-star" /></span>
-                    </h3>
-                    <p className="text-sm">Call me whenever you can...</p>
-                    <p className="text-sm text-muted"><i className="far fa-clock mr-1" /> 4 Hours Ago</p>
-                  </div>
-                </div>
-                {/* Message End */}
+    <div>
+      <div className="wrapper">
+        {/* Navbar */}
+        <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+          {/* Left navbar links */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" data-widget="pushmenu" href="#" role="button">
+                <i className="fas fa-bars" />
               </a>
-              {/* more messages... */}
-              <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item dropdown-footer">See All Messages</a>
-            </div>
-          </li>
-          {/* more navbar links... */}
-        </ul>
-      </nav>
-      {/* /.navbar */}
-      {/* rest of the code... */}
+            </li>
+            <li className="nav-item d-none d-sm-inline-block">
+              <a href="#" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item d-none d-sm-inline-block">
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+          {/* Right navbar links */}
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#" data-toggle="dropdown">
+                <i className="far fa-comments" />
+                <span className="badge badge-danger navbar-badge">3</span>
+              </a>
+              <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <a href="#" className="dropdown-item">
+                  {/* Message Start */}
+                  <div className="media">
+                    <img src="dist/img/user1-128x128.jpg" alt="User Avatar" className="img-size-50 mr-3 img-circle" />
+                    <div className="media-body">
+                      <h3 className="dropdown-item-title">
+                        Brad Diesel
+                        <span className="float-right text-sm text-danger">
+                          <i className="fas fa-star" />
+                        </span>
+                      </h3>
+                      <p className="text-sm">Call me whenever you can...</p>
+                      <p className="text-sm text-muted">
+                        <i className="far fa-clock mr-1" /> 4 Hours Ago
+                      </p>
+                    </div>
+                  </div>
+                  {/* Message End */}
+                </a>
+                {/* more messages... */}
+                <div className="dropdown-divider" />
+                <a href="#" className="dropdown-item dropdown-footer">
+                  See All Messages
+                </a>
+              </div>
+            </li>
+            {/* more navbar links... */}
+          </ul>
+        </nav>
+        {/* /.navbar */}
+        {/* rest of the code... */}
+      </div>
+      {/* ./wrapper */}
+      {/* required scripts... */}
     </div>
-    {/* ./wrapper */}
-    {/* required scripts... */}
-  </div>
-  )
-  {'}'}
-  export default Header
-</div>
+  );
+};
+
+export default Header;

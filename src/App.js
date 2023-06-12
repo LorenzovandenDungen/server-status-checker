c71307c97b8575877843d153d61c6cbbd0068a6a
LorenzovandenDungen <2130766@talnet.nl>
2023-06-12 12:57:01 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React from "react";
-import Header from "./components/Header";
-import SideNav from "./components/SideNav";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
+import React, { useEffect, useState } from 'react';
+import ServerStatus from './components/ServerStatus';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [serverData, setServerData] = useState([]);
+
+  useEffect(() => {
+    // Simulating API fetch call
+    const fetchServerStatus = async () => {
+      try {
+        // Make your API call here to fetch server status data
+        // Example using fetch:
+        const response = await fetch('YOUR_SERVER_STATUS_API_ENDPOINT');
+        const data = await response.json();
+        setServerData(data);
+      } catch (error) {
+        console.log('Error fetching server status:', error);
+      }
+    };
+
+    fetchServerStatus();
+  }, []);
+
   return (
-    <div>
-      <>
-      <Header />
-      <Home />
-      <SideNav />
-      <Footer />
-      </>
+    <div className="wrapper">
+      {/* Sidebar */}
+      <aside className="main-sidebar">
+        {/* Add your sidebar content here */}
+      </aside>
+
+      {/* Content */}
+      <div className="content-wrapper">
+        {/* Header */}
+        <header className="main-header">
+          {/* Add your header content here */}
+        </header>
+
+        {/* Page content */}
+        <div className="content">
+          <h1>Server Status Checker</h1>
+          {serverData.map(server => (
+            <ServerStatus
+              key={server.id}
+              name={server.name}
+              status={server.status}
+              details={server.details}
+            />
+          ))}
+        </div>
+
+        {/* Footer */}
+        <footer className="main-footer">
+          {/* Add your footer content here */}
+        </footer>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
